9b46f912b189340ce18c263c8404fc466c5319dd
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-06-08 17:31:13 -0700

    Adding first room

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import Bedroom from "./rooms/bedroom";
+import { ACESFilmicToneMapping } from "three";
 
 export default function App() {
 	return (
-		<Canvas shadows>
+		<Canvas
+			shadows
+			gl={{
+				toneMapping: ACESFilmicToneMapping,
+			}}>
 			<OrbitControls />
-			<ambientLight intensity={1.5} />
-			<mesh>
-				<boxGeometry />
-				<meshStandardMaterial color='hotpink' />
-			</mesh>
+			<ambientLight intensity={0.25} />
+			<directionalLight position={[0, 10, 0]} intensity={2} castShadow />
+			<Stage
+				preset='rembrandt'
+				// options: ["rembrandt", "portrait", "upfront", "soft"]
+				intensity={1}
+				contactShadow={true}
+				shadows>
+				<Bedroom />
+			</Stage>
 		</Canvas>
 	);
 }
