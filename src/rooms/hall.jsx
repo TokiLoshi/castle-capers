3eed9e977bd297667fce604006559ca95bd91c13
TokiLoshi <103384402+TokiLoshi@users.noreply.github.com>
2025-06-24 14:43:53 -0700

    finishing animations file

--- src/rooms/hall.jsx
+++ src/rooms/hall.jsx
@@ -5,6 +5,14 @@ import Man from "../characters/testimonies/Man";
 import Adventurer from "../characters/Testimonies/Adventurer";
 import HoodedAdventurer from "../characters/Testimonies/HoodedAdventurer";
 import Witch from "../characters/Testimonies/Witch";
+import AstroBee from "../characters/monsters/Astrobee";
+import AstroFrog from "../characters/Monsters/AstroFrog";
+import Bluey from "../characters/Monsters/Bluey";
+import Demon from "../characters/Monsters/Demon";
+import Fish from "../characters/Monsters/Fish";
+import Panda from "../characters/Monsters/Panda";
+import Ninja from "../characters/Ninja";
+import Zombie from "../characters/Zombie";
 
 export default function Hall() {
 	return (
@@ -14,7 +22,15 @@ export default function Hall() {
 			{/* <Adventurer position={[2, 0, 0]} /> */}
 			{/* <HoodedAdventurer position={[2, 0, 0]} /> */}
 			{/* <Man position={[2, 0, 0]} /> */}
-			<Witch position={[2, 0, 0]} />
+			{/* <Witch position={[2, 0, 0]} /> */}
+			{/* <AstroBee position={[2, 0, 0]} /> */}
+			{/* <AstroFrog position={[2, 0, 0]} /> */}
+			{/* <Bluey position={[2, 0, 0]} /> */}
+			{/* <Demon position={[2, 0, 0]} /> */}
+			{/* <Fish position={[2, 0, 0]} /> */}
+			{/* <Panda position={[2, 0, 0]} /> */}
+			{/* <Ninja position={[2, 0, 0]} /> */}
+			<Zombie position={[2, 0, 0]} />
 		</>
 	);
 }
